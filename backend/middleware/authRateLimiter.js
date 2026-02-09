27cdad199c2d9466f5581be602742a32118a63c0
BYTEZEN <shingh979875@gmail.com>
2026-02-09 12:36:36 +0530

    Added Bluetooth device scanner UI
    Signed-off-by: BYTEZEN <shingh979875@gmail.com>

---
+++ backend/middleware/authRateLimiter.js
@@ -0,0 +1,17 @@
+﻿const rateLimit = require('express-rate-limit');
+
+const authRateLimiter = (windowMs = 15 * 60 * 1000, max = 5) => {
+  return rateLimit({
+    windowMs,
+    max,
+    message: {
+      success: false,
+      message: 'Too many authentication attempts',
+      details: 'Please try again later'
+    },
+    standardHeaders: true,
+    legacyHeaders: false
+  });
+};
+
+module.exports = authRateLimiter;
