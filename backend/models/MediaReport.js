fb8ebaae735e9f5bd1eebd79b10f7ac019b24d90
BYTEZEN <shingh979875@gmail.com>
2026-02-21 22:32:07 +0530

    Created maintenance ticket creation API
    Signed-off-by: BYTEZEN <shingh979875@gmail.com>

---
+++ backend/models/MediaReport.js
@@ -0,0 +1,34 @@
+﻿const mongoose = require('mongoose');
+
+const mediaReportSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    index: true
+  },
+  appliance: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Appliance'
+  },
+  mediaType: {
+    type: String,
+
+enum: ['image', 'video', 'audio', 'document'],
+    required: true
+  },
+  fileUrl: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  }
+}, {
+  timestamps: true
+});
+
+mediaReportSchema.index({ user: 1, createdAt: -1 });
+mediaReportSchema.index({ appliance: 1 });
+
+module.exports = mongoose.model('MediaReport', mediaReportSchema);
