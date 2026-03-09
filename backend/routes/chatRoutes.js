69bccde3f3a021033c4b678a3d929335017f2a54
BYTEZEN <shingh979875@gmail.com>
2026-03-10 04:07:47 +0530

    Added thermal test UI
    Signed-off-by: BYTEZEN <shingh979875@gmail.com>

---
+++ backend/routes/chatRoutes.js
@@ -0,0 +1,21 @@
+﻿const express = require('express');
+const router = express.Router();
+const { protect } = require('../middleware/authMiddleware');
+const {
+  startChat,
+  sendMessage,
+  getChatHistory,
+  getChatSession
+} = require('../controllers/chatController');
+
+router.use(protect);
+
+router.post('/start', startChat);
+
+router.post('/:sessionId/message', sendMessage);
+
+router.get('/history', getChatHistory);
+
+router.get('/:sessionId', getChatSession);
+
+module.exports = router;
