8f48c751827cd50abe3f1a032215802ac200a946
BYTEZEN <shingh979875@gmail.com>
2026-03-18 18:23:08 +0530

    Added sensor reading normalization logic
    Signed-off-by: BYTEZEN <shingh979875@gmail.com>

---
+++ backend/routes/riskRoutes.js
@@ -0,0 +1,15 @@
+﻿const express = require('express');
+const router = express.Router();
+const { protect } = require('../middleware/authMiddleware');
+const {
+  calculateRiskScore,
+  getRiskHistory
+} = require('../controllers/riskController');
+
+router.use(protect);
+
+router.get('/calculate/:applianceId', calculateRiskScore);
+
+router.get('/history/:applianceId', getRiskHistory);
+
+module.exports = router;
