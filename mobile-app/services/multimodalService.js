2e890bffd346bd1b455f2a2a97265b7eca2d96e0
BYTEZEN <shingh979875@gmail.com>
2026-07-15 04:08:01 +0530

    Created status code guide
    Signed-off-by: BYTEZEN <shingh979875@gmail.com>

---
+++ mobile-app/services/multimodalService.js
@@ -0,0 +1,48 @@
+﻿import api from "./api";
+
+const unwrap = (r) => r?.data?.data ?? r?.data;
+
+export const analyzeMultimodal = async (payload) => {
+  const res = await api.post("/analyze/multimodal", payload);
+  return unwrap(res);
+};
+
+export const analyzeImage = async (payload) => {
+  const res = await api.post("/analyze/image", payload);
+  return unwrap(res);
+};
+
+export const analyzeVideo = async (payload) => {
+  const res = await api.post("/analyze/video", payload);
+  return unwrap(res);
+};
+
+export const analyzeAudio = async (payload) => {
+  const res = await api.post("/analyze/audio", payload);
+  return unwrap(res);
+};
+
+export const analyzeText = async (payload) => {
+  const res = await api.post("/analyze/text", payload);
+  return unwrap(res);
+};
+
+export const startTroubleshoot = async ({ deviceType, deviceName, initialIssue }) => {
+  const res = await api.post("/troubleshoot/start", { deviceType, deviceName, initialIssue });
+  return unwrap(res);
+};
+
+export const answerTroubleshoot = async (sessionId, { questionId, answer }) => {
+  const res = await api.post(`/troubleshoot/${sessionId}/answer`, { questionId, answer });
+  return unwrap(res);
+};
+
+export default {
+  analyzeMultimodal,
+  analyzeImage,
+  analyzeVideo,
+  analyzeAudio,
+  analyzeText,
+  startTroubleshoot,
+  answerTroubleshoot,
+};
