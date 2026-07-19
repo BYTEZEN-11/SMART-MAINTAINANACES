a2f809af0d32595f98a39869a52c308c843398a4
BYTEZEN <shingh979875@gmail.com>
2026-07-19 22:43:13 +0530

    Added biometric auth hook
    Signed-off-by: BYTEZEN <shingh979875@gmail.com>

---
+++ mobile-app/services/rulesService.js
@@ -0,0 +1,20 @@
+﻿import api from "./api";
+
+const unwrap = (r) => r?.data?.data ?? r?.data;
+
+export const getActiveRules = async (deviceType) => {
+  const res = await api.get("/rules/active", { params: { deviceType } });
+  return unwrap(res) || [];
+};
+
+export const evaluateRules = async ({ deviceType, evidence, analysis }) => {
+  const res = await api.post("/rules/evaluate", { deviceType, evidence, analysis });
+  return unwrap(res);
+};
+
+export const fireCounts = async (deviceType) => {
+  const res = await api.get("/rules/fire-counts", { params: { deviceType } });
+  return unwrap(res) || {};
+};
+
+export default { getActiveRules, evaluateRules, fireCounts };
