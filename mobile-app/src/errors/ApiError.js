05fb1861f8523a2c263cacd2ede251c984b119be
BYTEZEN <shingh979875@gmail.com>
2026-07-27 14:03:32 +0530

    Built success animation on actions
    Signed-off-by: BYTEZEN <shingh979875@gmail.com>

---
+++ mobile-app/src/errors/ApiError.js
@@ -0,0 +1,15 @@
+﻿
+
+export class ApiError extends Error {
+  constructor(message, { status = 0, data = null } = {}) {
+    super(message);
+    this.name = 'ApiError';
+    this.status = status;
+    this.data = data;
+  }
+}
+
+export const isUnauthorized = (err) => err?.status === 401;
+export const isConflict = (err) => err?.status === 409;
+export const isValidationError = (err) => err?.status === 400;
+export const isServerError = (err) => err?.status >= 500;
