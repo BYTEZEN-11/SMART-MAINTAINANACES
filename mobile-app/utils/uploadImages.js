cafd6bb923118559d974b524fb824c8fdec08836
BYTEZEN <shingh979875@gmail.com>
2026-08-02 07:33:46 +0530

    Built app icon and splash assets
    Signed-off-by: BYTEZEN <shingh979875@gmail.com>

---
+++ mobile-app/utils/uploadImages.js
@@ -0,0 +1,11 @@
+﻿import { uploadFile } from "../services/storageService";
+
+export const uploadImage = async (uri) => {
+  try {
+    const url = await uploadFile(uri, "image.jpg");
+    return url;
+  } catch (err) {
+    console.log("Upload Error:", err.message);
+    return null;
+  }
+};
